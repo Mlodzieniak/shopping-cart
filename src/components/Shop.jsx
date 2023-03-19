6d8c31b16a19d5f6700d1f63ab1f38e3eafab22c
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-03-19 18:12:30 +0100

    type button activate when route switches

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,38 +1,51 @@
 import React from "react";
 import { func } from "prop-types";
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route, useLocation } from "react-router-dom";
 import Items from "./Items";
 
 function Shop({ addToCart }) {
+  const location = useLocation();
+  const pickaxe = location.pathname === "/shop/pickaxes";
+  const glider = location.pathname === "/shop/gliders";
+  const backpack = location.pathname === "/shop/backpacks";
+  const isActive = (path) => (path ? "type-btn active-type" : "type-btn");
+
+  // const buttonClassName = isPickaxe ? "type-btn active-type" : "type-btn";
   return (
     <div className="shop">
-      <div className="types">
+      <div className="shop-types">
         <Link to="/shop/pickaxes">
-          <button type="button">Pickaxes</button>
+          <button type="button" className={isActive(pickaxe)}>
+            Pickaxes
+          </button>
         </Link>
         <Link to="/shop/gliders">
-          <button type="button">Gliders</button>
+          <button type="button" className={isActive(glider)}>
+            Gliders
+          </button>
         </Link>
         <Link to="/shop/backpacks">
-          <button type="button">Backpacks</button>
+          <button type="button" className={isActive(backpack)}>
+            Backpacks
+          </button>
         </Link>
       </div>
-      <div className="items">
-        <Routes>
-          <Route
-            path="/pickaxes"
-            element={<Items addToCart={addToCart} itemType="pickaxe" />}
-          />
-          <Route
-            path="/gliders"
-            element={<Items addToCart={addToCart} itemType="glider" />}
-          />
-          <Route
-            path="/backpacks"
-            element={<Items addToCart={addToCart} itemType="backpack" />}
-          />
-        </Routes>
-      </div>
+      {/* <div className="items"> */}
+      <Routes>
+        <Route
+          path="/pickaxes"
+          element={<Items addToCart={addToCart} itemType="pickaxe" />}
+        />
+        <Route
+          path="/gliders"
+          element={<Items addToCart={addToCart} itemType="glider" />}
+        />
+        <Route
+          path="/backpacks"
+          element={<Items addToCart={addToCart} itemType="backpack" />}
+        />
+      </Routes>
+      {/* </div> */}
     </div>
   );
 }
