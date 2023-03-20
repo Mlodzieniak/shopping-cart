6ba9dc4fe4118549652ddead2d88ed259e900158
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-03-21 00:48:14 +0100

    done about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,32 @@ import React from "react";
 function About() {
   return (
     <div className="about">
-      <h1>About Page!</h1>
+      <h1>Welcome to FN-Store</h1>
+      <p>
+        We&apos;re a team of passionate gamers who have been playing Fortnite
+        since the very beginning, and we&apos;re thrilled to share our love of
+        the game with you.
+      </p>
+      <p>
+        Our mission is to provide Fortnite players with everything they need to
+        gear up for victory. Whether you&apos;re a new player just starting out,
+        or a seasoned veteran looking for the latest and greatest items,
+        we&apos;ve got you covered. From skins and emotes to weapons and back
+        blings, we&apos;ve got a huge selection of items to choose from.
+      </p>
+      <p>
+        At FN-Store, we pride ourselves on providing exceptional customer
+        service. We&apos;re always here to answer your questions, help you find
+        the perfect item, or assist with any issues you may have. We believe in
+        treating our customers like family, and we&apos;re committed to ensuring
+        that you have the best possible shopping experience.
+      </p>
+      <p>
+        So what are you waiting for? Browse our selection of Fortnite items and
+        get ready to dominate the battlefield! If you have any questions or
+        feedback, feel free to contact us at any time. We can&apos;t wait to
+        hear from you.
+      </p>
     </div>
   );
 }
