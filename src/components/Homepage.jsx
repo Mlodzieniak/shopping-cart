660a6ba0aad6ebb4e149a4e7b3fbe3afd5a1d448
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-03-20 12:44:49 +0100

    video scrap

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import intro from "./video/intro.mp4";
 // import "./HomePage.css";
 
 function Homepage() {
   return (
-    <div className="video-background">
-      <video autoPlay muted loop>
-        <source src={intro} type="video/mp4" />
-      </video>
-      <div className="homepage">
-        <h1>Welcome to my website</h1>
-        <p>This is some text that will be displayed on top of the video</p>
-      </div>
+    <div className="homepage">
+      <h1>Welcome to my website</h1>
+      <p>This is some text that will be displayed on top of the video</p>
     </div>
   );
 }
